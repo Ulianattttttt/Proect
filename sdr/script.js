093c70becd8bf03c321070f59a01b43990fea46a
Uliana <uliana.t.a.2008@gmail.com>
2023-12-15 19:40:17 +0300

    commit

--- sdr/script.js
+++ sdr/script.js
@@ -20,3 +20,28 @@ if(men){
     inner += div
 }
 }
+
+function sendForm(event){
+    let error=true;
+    let adress = event.target[6].value;
+    if(adress.lenght < 10){
+        error.adress="Ваш адрес слишком короткий";
+        let addressField = document.getElementById("addres-error")
+        addressField.innerHTML= error.adress;
+        addressField.previousElementSibling.classList.add("error");
+    }
+    else{
+        error.adress=null;
+        let addressField = document.getElementById("addres-error")
+        addressField.innerHTML= '';
+        addressField.previousElementSibling.classList.remove("error");
+    }
+
+    let name = event.target[0].value;
+    let nameTemplate = /^[a-яА-Я\s]*[a-яА-Я]$/g;
+    if(!nameTemplate.test(name)){
+        error.name="Введите корректное имя"
+    }
+
+    return false
+}
